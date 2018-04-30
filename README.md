527003d56f664a22877cde1dd2e3ead282a3a158
Keigo Oka <ogiekako@gmail.com>
2018-05-01 00:13:53 +0900

    Make the lesson style customizable

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Set up `config.yaml`
 ```
 id: <dmm id>
 pass: <password>
-time: 06:00
+time: 08:30                       # the time to start the lesson
+lessonStyle: Free Conversation    # if omitted, defaults to Original Materials
 ```
 
-- `./reserve.py` reserves a lesson at the specified time of the next day
+- `./reserve.py` reserves a lesson at the specified time of the nearest day
 - `./setup.sh` sets up cronjob to reserve a lesson every day
--- reserve.py
+++ reserve.py
@@ -12,7 +12,24 @@ import yaml
 
 f = open(
     os.path.join(os.getcwd(), os.path.dirname(__file__), 'config.yaml'), 'r+')
-data = yaml.load(f)
+pref = yaml.load(f)
+
+pref_id = pref['id']
+pref_pass = pref['pass']
+pref_time = pref['time']
+pref_lesson_style = pref['lessonStyle'] if 'lessonStyle' in pref else 'Original Materials'
+pref_original_material = ''
+if pref_lesson_style == 'Original Materials':
+  pref_original_material = pref['originalMaterial'] if 'originalMaterial' in pref else 'Daily News'
+
+def get_target_datetime(time_str):
+    t = datetime.datetime.strptime(time_str, '%H:%M')
+    target_datetime = datetime.datetime.combine(datetime.date.today(), datetime.time(hour = t.hour, minute = t.minute))
+    while target_datetime < datetime.datetime.now():
+        target_datetime += datetime.timedelta(days = 1)
+    return target_datetime
+
+target_datetime = get_target_datetime(pref_time);
 
 options = webdriver.ChromeOptions()
 if '--headless' in sys.argv:
@@ -24,16 +41,13 @@ driver = webdriver.Chrome(chrome_options=options)
 driver.get(
     "https://www.dmm.com/my/-/login/=/path=DRVESVwZTldRDlBRRFdIUwwIGFVfVEs_")
 login_id = driver.find_element_by_id("login_id")
-login_id.send_keys(data['id'])
+login_id.send_keys(pref_id)
 password = driver.find_element_by_id("password")
-password.send_keys(data['pass'])
+password.send_keys(pref_pass)
 driver.find_element_by_tag_name("form").submit()
 
-time = data['time']
-day = datetime.datetime.today()
-
 tmpl = "http://eikaiwa.dmm.com/list/?data[tab1][start_time]={}&data[tab1][end_time]={}&data[tab1][gender]=0&data[tab1][age]=年齢&data[tab1][free_word]=&date={}&tab=0&sort=4"
-url = tmpl.format(time, time, day.strftime('%Y-%m-%d'))
+url = tmpl.format(pref_time, pref_time, target_datetime.strftime('%Y-%m-%d'))
 driver.get(url)
 
 buttons = driver.find_elements_by_class_name('bt-open')
@@ -48,9 +62,10 @@ for button in buttons:
             EC.url_contains('eikaiwa.dmm.com/book/index'))
 
         driver.find_element_by_id('lessonStyle').click()
-        driver.find_element_by_css_selector('#lessonStyle > [value = "Original Materials"]').click()
-        driver.find_element_by_id('originalMaterial').click()
-        driver.find_element_by_css_selector('#originalMaterial > [value = "Daily News"]').click()
+        driver.find_element_by_css_selector('#lessonStyle > [value = "{}"]'.format(pref_lesson_style)).click()
+        if pref_original_material:
+            driver.find_element_by_id('originalMaterial').click()
+            driver.find_element_by_css_selector('#originalMaterial > [value = "{}"]'.format(pref_original_material)).click()
 
         driver.find_element_by_id('submitBox').submit()
         break
